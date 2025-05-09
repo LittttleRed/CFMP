5cafbf9978abcd1b2bc2ef6bcd15e24569f50d68
LittttleRed <2917781292@qq.com>
2025-05-09 20:14:34 +0800

    serializer init and add price

--- 后端/cfmp/root/views.py
+++ 后端/cfmp/root/views.py
@@ -1,26 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from ..model import models
+from model.models import User,Product
+from model.serializers import UserSerializer,ProductSerializer
+
+
 @api_view(['PUT'])
 def admin_user_get(request):
     return Response({'code': "200", 'message':'admin_user_get'})
 # Create your views here.
+
+
 def test(request):
-    params = request.query_params.dict()
+    params = request.GET.dict()
 
     # 初始化查询集
-    queryset = models.User.objects.all()
-
+    queryset = User.objects.all()
+    
     # 动态过滤（自动匹配模型字段）
     if params:
         # 过滤参数需要是模型的字段
-        valid_fields = [f.name for f in models.User._meta.get_fields()]
+        valid_fields = [f.name for f in User._meta.get_fields()]
         filtered_params = {k: v for k, v in params.items() if k in valid_fields}
 
         # 使用 ** 解包参数进行过滤
         queryset = queryset.filter(**filtered_params)
-
+        for i in queryset:
+            print(UserSerializer(data=i))
+        
     # 序列化并返回结果
 
-    return None
+    return HttpResponse(queryset)
