3b235482747bf57b19b536c0e8b3bf4c2565551c
LittttleRed <2917781292@qq.com>
2025-05-17 20:50:59 +0800

    minio

--- 后端/cfmp/user/views.py
+++ 后端/cfmp/user/views.py
@@ -1,10 +1,16 @@
-
+import urllib3
+from django.core.files.storage import default_storage
+from django.shortcuts import render
 from minio_storage import MinioMediaStorage
 from rest_framework import status
-
+from rest_framework.decorators import api_view
+from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from .serializers import UserSerializer, ImageSerializer
+from .models import User,Image
+from minio import Minio
 
 # Create your views here.
 
