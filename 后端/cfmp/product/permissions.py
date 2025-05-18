f10f905b4c09cfb496d74216b2b8a318e59222ed
LittttleRed <2917781292@qq.com>
2025-05-18 15:52:19 +0800

    minio

--- 后端/cfmp/product/permissions.py
+++ 后端/cfmp/product/permissions.py
@@ -6,7 +6,6 @@ class IsOwnerOrReadOnly(BasePermission):
     通用权限类：对于GET等只读操作允许任何人访问
     对于修改和删除操作，只允许资源的拥有者
     """
-
     message = "您没有权限执行此操作"
 
     def has_object_permission(self, request, view, obj):
