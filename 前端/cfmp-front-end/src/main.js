94da2bd62f8f21170cf0ac35e632aa6bafe95cfd
LittttleRed <2917781292@qq.com>
2025-05-20 18:01:27 +0800

    product card

--- 前端/cfmp-front-end/src/main.js
+++ 前端/cfmp-front-end/src/main.js
@@ -3,21 +3,8 @@ import ElementPlus from 'element-plus'
 import router from './router'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
-<<<<<<< HEAD
-import router from './router'
-import { createPinia } from 'pinia'
-
-import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
-const app = createApp(App)
-
-app.use(ElementPlus)
-app.use(router)
-app.use(createPinia())
-app.mount('#app')
-=======
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import pinia from './stores/index.js'
 const app = createApp(App)
@@ -28,4 +15,4 @@ app.use(ElementPlus, {
 app.use(router)
 app.use(pinia)
 app.mount('#app')
->>>>>>> 11b53feeb6e16b34efa49e9a9e53ae43242ac49b
+
