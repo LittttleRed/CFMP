758fb62e277a2e7fa2088b2018ee8425d1eab39d
雷家兴 <leij49456@gmail.com>
2025-05-21 18:22:14 +0800

    📝 docs(model): add docs for product's models
    docs pop up when mouse suspending

--- 后端/cfmp/product/models.py
+++ 后端/cfmp/product/models.py
@@ -4,6 +4,19 @@ from django_minio_backend import MinioBackend
 
 
 class Product(models.Model):
+    """Product
+
+    Attributes:
+        product_id: primary_key(not nessary)
+        user: model
+        title: CharField
+        description: TextField
+        price: DecimalField
+        status:  default=0
+        created_at: DateTimeField(not nessary)
+        categories: model(related_name="products")
+    """
+
     product_id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=100)
@@ -18,6 +31,14 @@ class Product(models.Model):
 
 
 class ProductMedia(models.Model):
+    """ProductMedia
+
+    Attributes:
+        media_id: primary_key(not nessary)
+        product: model(related_name="media")
+        media: FileField
+    """
+
     media_id = models.BigAutoField(primary_key=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="media")
     media = models.FileField(
@@ -27,8 +48,18 @@ class ProductMedia(models.Model):
         blank=True,
     )
 
+    class Meta:
+        da_table = "product_media"
+
 
 class Category(models.Model):
+    """Category
+
+    Attributes:
+        category_id: primary_key(not nessary)
+        name: CharField
+    """
+
     category_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=50)
 
@@ -37,6 +68,17 @@ class Category(models.Model):
 
 
 class ProductReview(models.Model):
+    """ProductReview
+
+    Attributes:
+        review_id: primary_key(not nessary)
+        product: model(related_name="reviews")
+        user: model
+        rating: 1-5
+        comment: TextField
+        created_at: DateTimeField(not nessary)
+    """
+
     review_id = models.BigAutoField(primary_key=True)
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name="reviews"
@@ -52,6 +94,14 @@ class ProductReview(models.Model):
 
 
 class ProductReviewMedia(models.Model):
+    """ProductReviewMedia
+
+    Attributes:
+        review_media_id: primary_key(not nessary)
+        review: model(related_name="media")
+        media: FileField
+    """
+
     review_media_id = models.BigAutoField(primary_key=True)
     review = models.ForeignKey(
         ProductReview, on_delete=models.CASCADE, related_name="media"
@@ -63,8 +113,19 @@ class ProductReviewMedia(models.Model):
         blank=True,
     )
 
+    class Meta:
+        db_table = "product_review_media"
+
 
 class Collection(models.Model):
+    """Collection
+
+    Attributes:
+        collection: model(related_name="collections")
+        collecter: model
+        create_at: DateTimeField(not nessary)
+    """
+
     collection = models.ForeignKey(Product, on_delete=models.CASCADE)
     collecter = models.ForeignKey(User, on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
