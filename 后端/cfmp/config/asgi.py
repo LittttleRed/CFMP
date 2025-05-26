8049b5ef357c11c7aa4a353d55f63508cf76b552
LittttleRed <2917781292@qq.com>
2025-05-26 20:24:34 +0800

    关注bug修改

--- 后端/cfmp/config/asgi.py
+++ 后端/cfmp/config/asgi.py
@@ -7,21 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
-import os
 
+import os
 from channels.routing import ProtocolTypeRouter,URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import user.routing
+
+
+django_asgi_app = get_asgi_application()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+import user.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "http": django_asgi_app,
+    "websocket":
         URLRouter(
             # 在这里添加你的WebSocket路由
             user.routing.websocket_urlpatterns
         )
-    )
 })
