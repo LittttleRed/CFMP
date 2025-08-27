cd1b5ba03b8bf8e5457a3c15cf1252c8447222ab
LittttleRed <2917781292@qq.com>
2025-08-27 09:26:35 +0800

    apt
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt
    
    apt

--- start-k8s.sh
+++ start-k8s.sh
@@ -3,8 +3,7 @@
 echo "🚀 启动 CFMP Kubernetes 应用..."
 
 # 启动 minikube
-echo "启动 minikube..."
-minikube start
+
 
 # 配置 Docker 环境
 eval $(minikube docker-env)
@@ -18,18 +17,18 @@ cd 前端/cfmp-front-end && docker build -t frontend . && cd ../..
 
 # 部署应用
 echo "部署应用..."
-kubectl delete -f k8s/ --ignore-not-found=true
+minikube kubectl -- delete -f k8s/ --ignore-not-found=true
 sleep 3
-kubectl create -f k8s/
+minikube kubectl -- create -f k8s/
 
 # 等待启动
 echo "等待应用启动..."
-kubectl wait --for=condition=ready pod -l io.kompose.service=backend --timeout=300s
-kubectl wait --for=condition=ready pod -l io.kompose.service=frontend --timeout=300s
+minikube kubectl -- wait --for=condition=ready pod -l io.kompose.service=backend --timeout=300s
+minikube kubectl -- wait --for=condition=ready pod -l io.kompose.service=frontend --timeout=300s
 
 # 暴露服务
-kubectl patch service frontend -p '{"spec":{"type":"NodePort"}}'
-kubectl patch service backend -p '{"spec":{"type":"NodePort"}}'
+minikube kubectl -- patch service frontend -p '{"spec":{"type":"NodePort"}}'
+minikube kubectl -- patch service backend -p '{"spec":{"type":"NodePort"}}'
 
 # 显示访问地址
 echo ""
