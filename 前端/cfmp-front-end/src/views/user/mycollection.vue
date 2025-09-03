75500011820fce26fe9b4766fe152b785abefa9d
蒋雨宸 <23373059>
2025-09-03 16:16:43 +0800

    我的收藏修正

--- 前端/cfmp-front-end/src/views/user/mycollection.vue
+++ 前端/cfmp-front-end/src/views/user/mycollection.vue
@@ -13,9 +13,9 @@
         <Product 
           :title="product.title" 
           :price="product.price" 
-          :avatar="product.user?.avatar || ''" 
-          :username="product.user?.username || ''" 
-          :user_id="product.user?.user_id || ''" 
+          :avatar="product.user_info?.avatar || ''" 
+          :username="product.user_info?.username || ''" 
+          :user_id="product.user_info?.user_id || ''" 
           :product_id="product.product_id" 
           :media="product.media && product.media[0] ? product.media[0].media : null">
         </Product>
@@ -35,10 +35,16 @@ const getCollections=async()=>{
   try {
     loading.value = true;
     const res=await getMyCollections(getToken());
-    console.log(res.results);
-    productList.value.push(...res.results.map(item => item.collection))
+    console.log("收藏列表响应:", res);
+    // 正确处理收藏数据，从collection字段中提取商品信息
+    productList.value = res.results.map(item => {
+      // 后端返回的数据结构是 {collection: {...商品信息...}, collecter: {...收藏者信息...}}
+      // 我们需要的是商品信息，它在collection字段中
+      return item.collection || {};
+    });
   } catch (error) {
     console.error("获取收藏列表失败:", error);
+    productList.value = []; // 出错时清空列表
   } finally {
     loading.value = false;
   }
